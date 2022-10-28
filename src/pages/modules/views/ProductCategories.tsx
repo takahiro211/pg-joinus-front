f84d90f97dfffd4531b45228db0e5e93d84f3345
takahiro211 <ytakahiro211@gmail.com>
2022-10-29 00:33:28 +0900

    Mod: プロジェクト一覧をスライド式カードに変更

--- src/pages/modules/views/ProductCategories.tsx
+++ src/pages/modules/views/ProductCategories.tsx
@@ -5,6 +5,12 @@ import Container from "@mui/material/Container";
 import Typography from "../components/Typography";
 import { Labels } from "../../../utils/Consts";
 import {
+  Button,
+  Card,
+  CardActionArea,
+  CardActions,
+  CardContent,
+  Chip,
   Paper,
   Table,
   TableBody,
@@ -17,102 +23,9 @@ import { DateFormat } from "../../../utils/Util";
 import { PostsEntity } from "../../../api/entities/PostsEntity";
 import { useEffect, useState } from "react";
 import { RepositoryFactory } from "../../../api/RepositoryFactory";
-
-const ImageBackdrop = styled("div")(({ theme }) => ({
-  position: "absolute",
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  background: "#000",
-  opacity: 0.5,
-  transition: theme.transitions.create("opacity"),
-}));
-
-const ImageIconButton = styled(ButtonBase)(({ theme }) => ({
-  position: "relative",
-  display: "block",
-  padding: 0,
-  borderRadius: 0,
-  height: "40vh",
-  [theme.breakpoints.down("md")]: {
-    width: "100% !important",
-    height: 100,
-  },
-  "&:hover": {
-    zIndex: 1,
-  },
-  "&:hover .imageBackdrop": {
-    opacity: 0.15,
-  },
-  "&:hover .imageMarked": {
-    opacity: 0,
-  },
-  "&:hover .imageTitle": {
-    border: "4px solid currentColor",
-  },
-  "& .imageTitle": {
-    position: "relative",
-    padding: `${theme.spacing(2)} ${theme.spacing(4)} 14px`,
-  },
-  "& .imageMarked": {
-    height: 3,
-    width: 18,
-    background: theme.palette.common.white,
-    position: "absolute",
-    bottom: -2,
-    left: "calc(50% - 9px)",
-    transition: theme.transitions.create("opacity"),
-  },
-}));
-
-const images = [
-  {
-    url: "https://images.unsplash.com/photo-1534081333815-ae5019106622?auto=format&fit=crop&w=400",
-    title: "Snorkeling",
-    width: "40%",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1531299204812-e6d44d9a185c?auto=format&fit=crop&w=400",
-    title: "Massage",
-    width: "20%",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1476480862126-209bfaa8edc8?auto=format&fit=crop&w=400",
-    title: "Hiking",
-    width: "40%",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1453747063559-36695c8771bd?auto=format&fit=crop&w=400",
-    title: "Tour",
-    width: "38%",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1523309996740-d5315f9cc28b?auto=format&fit=crop&w=400",
-    title: "Gastronomy",
-    width: "38%",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1534452203293-494d7ddbf7e0?auto=format&fit=crop&w=400",
-    title: "Shopping",
-    width: "24%",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1506941433945-99a2aa4bd50a?auto=format&fit=crop&w=400",
-    title: "Walking",
-    width: "40%",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1533727937480-da3a97967e95?auto=format&fit=crop&w=400",
-    title: "Fitness",
-    width: "20%",
-  },
-  {
-    url: "https://images.unsplash.com/photo-1518136247453-74e7b5265980?auto=format&fit=crop&w=400",
-    title: "Reading",
-    width: "40%",
-  },
-];
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 export default function ProductCategories() {
   const [posts, setPosts] = useState<PostsEntity[]>([]);
@@ -133,40 +46,80 @@ export default function ProductCategories() {
     }
   };
 
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    autoplay: true,
+    speed: 320,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    pauseOnHover: true,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 960,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
   return (
     <Container component="section" sx={{ mt: 8, mb: 4 }}>
       <Typography variant="h4" marked="center" align="center" component="h2">
         {Labels.LATEST_PROJECT_LIST}
       </Typography>
       <Box sx={{ mt: 7, mb: 12 }}>
-        <TableContainer component={Paper} sx={{ mt: 4 }}>
-          <Table aria-label="simple table">
-            <TableHead>
-              <TableRow>
-                <TableCell>{Labels.USERS_TABEL_HEAD_POST_TITLE}</TableCell>
-                <TableCell>
-                  {Labels.USERS_TABEL_HEAD_POST_DESCRIPTION}
-                </TableCell>
-                <TableCell>{Labels.USERS_TABEL_HEAD_POST_DATE}</TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>
-              {posts.map((post) => (
-                <TableRow>
-                  <TableCell component="th" scope="row">
-                    {post.title}
-                  </TableCell>
-                  <TableCell component="th" scope="row">
-                    {post.description}
-                  </TableCell>
-                  <TableCell component="th" scope="row">
-                    {DateFormat(post.created_at)}
-                  </TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
-        </TableContainer>
+        <Slider {...sliderSettings}>
+          {posts.map((post) => (
+            <>
+              <Card
+                sx={{ minWidth: 10, mt: 2, mb: 2 }}
+                style={{ marginRight: 12, marginLeft: 12 }}
+              >
+                <CardActionArea>
+                  <CardContent>
+                    <Typography
+                      sx={{ fontSize: 14 }}
+                      color="text.secondary"
+                      gutterBottom
+                    >
+                      {DateFormat(post.created_at)}
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                      {post.title}
+                    </Typography>
+                    <Typography sx={{ mb: 1.5 }} color="text.secondary">
+                      {post.description}
+                    </Typography>
+                    <Typography variant="body2">
+                      {JSON.parse(post.skill).map((skill: any) => (
+                        <Chip
+                          variant="outlined"
+                          color="success"
+                          size="small"
+                          label={skill}
+                          sx={{ mt: 0.5, mr: 0.4 }}
+                        />
+                      ))}
+                      {JSON.parse(post.free_tag).map((tag: any) => (
+                        <Chip
+                          variant="outlined"
+                          size="small"
+                          label={tag}
+                          sx={{ mt: 0.5, mr: 0.4 }}
+                        />
+                      ))}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+              <Box sx={{ ml: 12, mr: 12 }}></Box>
+            </>
+          ))}
+        </Slider>
       </Box>
     </Container>
   );
